9d1de04518414bb901833dbe58b623e0f90a1d58
Winston Liu <Wliu1402@gmail.com>
2022-12-04 17:03:24 -0500

    2022, Day 2 - Rock Paper Scissors

---
+++ 2022/src/day_02.rs
@@ -0,0 +1,114 @@
+use std::{fs, cmp::Ordering};
+
+pub fn part_1() -> u32 {
+    let contents = fs::read_to_string("day_02_input.txt")
+        .expect("Something went wrong reading the file");
+
+    contents.lines().map(|line| {
+        line.split_whitespace().collect::<Vec<&str>>()
+    }).fold(0, |accum, round| {
+        let opponent_shape = match round[0] {
+            "A" => Shape::Rock,
+            "B" => Shape::Paper,
+            "C" => Shape::Scissors,
+            _ => panic!("Invalid opponent shape"),
+        };
+        let self_shape = match round[1] {
+            "X" => Shape::Rock,
+            "Y" => Shape::Paper,
+            "Z" => Shape::Scissors,
+            _ => panic!("Invalid self shape"),
+        };
+
+        let result = self_shape.cmp(&opponent_shape);
+        accum + self_shape as u32 + match result {
+            Ordering::Less => 0,
+            Ordering::Equal => 3,
+            Ordering::Greater => 6,
+        }
+    })
+}
+
+pub fn part_2() -> u32 {
+    let contents = fs::read_to_string("day_02_input.txt")
+        .expect("Something went wrong reading the file");
+
+    contents.lines().map(|line| {
+        line.split_whitespace().collect::<Vec<&str>>()
+    }).fold(0, |accum, round| {
+        let opponent_shape = match round[0] {
+            "A" => Shape::Rock,
+            "B" => Shape::Paper,
+            "C" => Shape::Scissors,
+            _ => panic!("Invalid opponent shape"),
+        };
+        let outcome = match round[1] {
+            "X" => Outcome::Lose,
+            "Y" => Outcome::Draw,
+            "Z" => Outcome::Win,
+            _ => panic!("Invalid outcome"),
+        };
+
+        let self_shape = Shape::from_i32((opponent_shape as i32 + outcome as i32) % 3);
+
+        let result = self_shape.cmp(&opponent_shape);
+        accum + self_shape as u32 + match result {
+            Ordering::Less => 0,
+            Ordering::Equal => 3,
+            Ordering::Greater => 6,
+        }
+    })
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+enum Shape {
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
+}
+
+impl Shape {
+    fn from_i32(value: i32) -> Shape {
+        match value {
+            0 => Shape::Scissors, // Edge case since we're one-indexed
+            1 => Shape::Rock,
+            2 => Shape::Paper,
+            3 => Shape::Scissors,
+            _ => panic!("Invalid shape"),
+        }
+    }
+}
+
+impl Ord for Shape {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self {
+            Shape::Rock => match other {
+                Shape::Rock => Ordering::Equal,
+                Shape::Paper => Ordering::Less,
+                Shape::Scissors => Ordering::Greater,
+            },
+            Shape::Paper => match other {
+                Shape::Rock => Ordering::Greater,
+                Shape::Paper => Ordering::Equal,
+                Shape::Scissors => Ordering::Less,
+            },
+            Shape::Scissors => match other {
+                Shape::Rock => Ordering::Less,
+                Shape::Paper => Ordering::Greater,
+                Shape::Scissors => Ordering::Equal,
+            },
+        }
+    }
+}
+
+impl PartialOrd for Shape {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+enum Outcome {
+    Lose = -1,
+    Draw = 0,
+    Win = 1,
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -1,6 +1,9 @@
 pub mod day_01;
+pub mod day_02;
 
 fn main() {
     println!("{}", day_01::part_1());
     println!("{}", day_01::part_2());
+    println!("{}", day_02::part_1());
+    println!("{}", day_02::part_2());
 }
