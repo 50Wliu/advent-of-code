7a8de286ecdc0856167e2ed9eb3f7c7717e1d921
Austin Sullivan <austin4sullivan@gmail.com>
2023-12-03 06:11:06 +0000

    gitignore inputs
    allows the source to be more easily shared

--- 2023/src/utilities.rs
+++ 2023/src/utilities.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
 pub fn read_input(day: u8) -> String {
-    let path = format!("day_{:02}_input.txt", day);
+    let path = format!("inputs/day_{:02}.txt", day);
     fs::read_to_string(path).expect("Something went wrong reading the file")
 }
