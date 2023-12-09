10a645d8c6d820bfe3cdcc9e406145ee941de965
Winston Liu <Wliu1402@gmail.com>
2023-12-08 23:35:30 -0800

    Day 2: ok_or, try_fold, HashMap

--- 2023/src/day_02.rs
+++ 2023/src/day_02.rs
@@ -1,4 +1,4 @@
-use std::str::FromStr;
+use std::{str::FromStr, collections::HashMap};
 
 pub fn part_1(contents: &str) -> Result<u32, String> {
     let bag = Set {
@@ -53,16 +53,15 @@ impl FromStr for Game {
     fn from_str(s: &str) -> Result<Game, Self::Err> {
         // Game 1: 4 blue, 16 green, 2 red; 5 red, 11 blue, 16 green; 9 green, 11 blue; 10 blue, 6 green, 4 red
         let mut segments = s.split(':');
-        if let (Some(game), Some(rest)) = (segments.next(), segments.next()) {
-            if let (Ok(id), Ok(sets)) = (game[5..].parse::<u32>(), rest.split(';').map(|set| set.parse::<Set>()).into_iter().collect::<Result<Vec<_>,_>>()) {
-                return Ok(Game {
-                    id,
-                    sets,
-                })
-            }
-        }
-
-        Err("Invalid game".to_string())
+        let game = segments.next().ok_or("Missing game".to_string())?;
+        let sets = segments.next().ok_or("Missing sets".to_string())?;
+        let id = game.split_whitespace().skip(1).next().ok_or("Missing game id".to_string())?
+            .parse::<u32>().map_err(|err| err.to_string())?;
+        let sets = sets.split(';').map(|set| set.parse::<Set>()).collect::<Result<Vec<_>,_>>()?;
+        Ok(Game {
+            id,
+            sets,
+        })
     }
 }
 
@@ -71,25 +70,21 @@ impl FromStr for Set {
     fn from_str(s: &str) -> Result<Set, Self::Err> {
         // 4 blue, 16 green, 2 red
 
-        let mut result = Set {
-            red: 0,
-            green: 0,
-            blue: 0,
-        };
-
-        let mut colors = s.split(',');
-        while let Some(color) = colors.next() {
-            let mut num_and_color = color.split_whitespace();
-            if let (Some(raw_num), Some(color)) = (num_and_color.next(), num_and_color.next()) {
-                let num = raw_num.parse::<u32>().map_err(|err| err.to_string())?;
-                match color {
-                    "red" => result.red = num,
-                    "green" => result.green = num,
-                    "blue" => result.blue = num,
-                    _ => panic!()
-                }
+        let colors = s.split(',').try_fold(HashMap::new(), |mut acc, segment| {
+            let mut colors = segment.split_whitespace();
+            let num = colors.next().ok_or("Missing number".to_string())?
+                .parse::<u32>().map_err(|err| err.to_string())?;
+            let color = colors.next().ok_or("Missing color".to_string())?;
+            match acc.insert(color, num) {
+                Some(_) => Err(format!("Duplicate color {}", color).to_string()),
+                None => Ok(acc),
             }
-        }
-        Ok(result)
+        })?;
+
+        Ok(Set {
+            red: *colors.get("red").ok_or("Missing red".to_string())?,
+            green: *colors.get("green").ok_or("Missing green".to_string())?,
+            blue: *colors.get("blue").ok_or("Missing blue".to_string())?,
+        })
     }
 }
