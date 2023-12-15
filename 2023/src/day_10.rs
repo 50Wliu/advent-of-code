588a7e74e72ddd92881135790684c24deb8712a2
Winston Liu <Wliu1402@gmail.com>
2023-12-15 00:12:13 -0800

    Split into two functions

--- 2023/src/day_10.rs
+++ 2023/src/day_10.rs
@@ -17,7 +17,7 @@ pub fn part_1(contents: &str) -> Result<u64, String> {
 
     let grid = lines.collect::<Vec<_>>();
 
-    let steps = traverse(&grid, START, Direction::None, row, col).ok_or("No path found".to_string())?;
+    let steps = start_traverse(&grid, row, col);
 
     // Farthest distance will be at the halfway point.
     Ok(steps / 2)
@@ -29,11 +29,52 @@ pub fn part_2(contents: &str) -> Result<u64, String> {
     Ok(0)
 }
 
+fn start_traverse(grid: &[&str], row: usize, col: usize) -> u64 {
+    if let Some(line) = grid.get(row - 1) {
+        if let Some(char) = line.chars().nth(col) {
+            if char == NORTH_SOUTH || char == SOUTH_WEST || char == SOUTH_EAST {
+                if let Some(steps) = traverse(grid, char, Direction::South, row - 1, col) {
+                    return steps;
+                }
+            }
+        }
+    }
+
+    if let Some(line) = grid.get(row + 1) {
+        if let Some(char) = line.chars().nth(col) {
+            if char == NORTH_SOUTH || char == NORTH_WEST || char == NORTH_EAST {
+                if let Some(steps) = traverse(grid, char, Direction::North, row + 1, col) {
+                    return steps;
+                }
+            }
+        }
+    }
+
+    if let Some(char) = grid[row].chars().nth(col - 1) {
+        if char == EAST_WEST || char == NORTH_EAST || char == SOUTH_EAST {
+            if let Some(steps) = traverse(grid, char, Direction::East, row, col - 1) {
+                return steps;
+            }
+        }
+    }
+
+    if let Some(char) = grid[row].chars().nth(col + 1) {
+        if char == EAST_WEST || char == NORTH_WEST || char == SOUTH_WEST {
+            if let Some(steps) = traverse(grid, char, Direction::West, row, col + 1) {
+                return steps;
+            }
+        }
+    }
+
+    panic!("No path found");
+}
+
+// TODO:
+// * Change to iterative.
 fn traverse(grid: &[&str], char: char, from: Direction, row: usize, col: usize) -> Option<u64> {
     // If the current pipe can connect to the north,
     // see if the pipe above can also connect to the south.
-    if from != Direction::North
-        && (char == START || char == NORTH_SOUTH || char == NORTH_WEST || char == NORTH_EAST)
+    if from != Direction::North && (char == NORTH_SOUTH || char == NORTH_WEST || char == NORTH_EAST)
     {
         if let Some(line) = grid.get(row - 1) {
             if let Some(char) = line.chars().nth(col) {
@@ -48,12 +89,13 @@ fn traverse(grid: &[&str], char: char, from: Direction, row: usize, col: usize) 
                 }
             }
         }
+
+        return None;
     }
 
     // If the current pipe can connect to the south,
     // see if the pipe below can also connect to the north.
-    if from != Direction::South
-        && (char == START || char == NORTH_SOUTH || char == SOUTH_WEST || char == SOUTH_EAST)
+    if from != Direction::South && (char == NORTH_SOUTH || char == SOUTH_WEST || char == SOUTH_EAST)
     {
         if let Some(line) = grid.get(row + 1) {
             if let Some(char) = line.chars().nth(col) {
@@ -68,13 +110,13 @@ fn traverse(grid: &[&str], char: char, from: Direction, row: usize, col: usize) 
                 }
             }
         }
+
+        return None;
     }
 
     // If the current pipe can connect to the west,
     // see if the pipe to the left can also connect to the east.
-    if from != Direction::West
-        && (char == START || char == EAST_WEST || char == NORTH_WEST || char == SOUTH_WEST)
-    {
+    if from != Direction::West && (char == EAST_WEST || char == NORTH_WEST || char == SOUTH_WEST) {
         if let Some(char) = grid[row].chars().nth(col - 1) {
             if char == START {
                 return Some(0);
@@ -86,13 +128,13 @@ fn traverse(grid: &[&str], char: char, from: Direction, row: usize, col: usize) 
                 }
             }
         }
+
+        return None;
     }
 
     // If the current pipe can connect to the east,
     // see if the pipe to the right can also connect to the west.
-    if from != Direction::East
-        && (char == START || char == EAST_WEST || char == NORTH_EAST || char == SOUTH_EAST)
-    {
+    if from != Direction::East && (char == EAST_WEST || char == NORTH_EAST || char == SOUTH_EAST) {
         if let Some(char) = grid[row].chars().nth(col + 1) {
             if char == START {
                 return Some(0);
@@ -104,16 +146,17 @@ fn traverse(grid: &[&str], char: char, from: Direction, row: usize, col: usize) 
                 }
             }
         }
+
+        return None;
     }
 
-    None
+    // None
 
-    // panic!("No connector found at {}, {}", row, col);
+    panic!("Pipe `{}` at {}, {} cannot connect to anything coming from direction {:?}", char, row, col, from);
 }
 
 #[derive(Debug, PartialEq)]
 enum Direction {
-    None,
     North,
     South,
     East,
